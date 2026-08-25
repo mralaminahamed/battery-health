2a8663c946935188decbaa7db93da813a8682c35
Al Amin Ahamed <alamin.ahamed.dev@gmail.com>
2026-08-25 12:59:36 +0600

    Release 0.1.1 with the socket crash fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,8 +53,8 @@ android {
         applicationId = "com.alaminahamed.batteryhealth"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
